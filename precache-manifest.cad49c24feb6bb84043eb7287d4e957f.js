2db9d2b0b6191f4308fc35a5ce31397c3530c28d
avirupdutta <finalguider99@gmail.com>
2020-06-03 19:34:32 +0530

    pokedex is ready to deploy!

---
+++ precache-manifest.cad49c24feb6bb84043eb7287d4e957f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b0d6d0f844cffb57dda4be6222bfbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1080ceb97c30c06ee99a",
+    "url": "/static/css/main.c1a7c2c9.chunk.css"
+  },
+  {
+    "revision": "1beba42241dd34261d64",
+    "url": "/static/js/2.9480692f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9480692f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1080ceb97c30c06ee99a",
+    "url": "/static/js/main.1500eb6a.chunk.js"
+  },
+  {
+    "revision": "7250a77dc59ce6b6d93d",
+    "url": "/static/js/runtime-main.59da08b4.js"
+  }
+]);
